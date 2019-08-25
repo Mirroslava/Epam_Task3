7a2157750b1788b63a37a33565d6791cfdbd769f
Mirroslava <mirosya.barabash@gmail.com>
2019-08-25 15:49:45 +0300

    first and second task

---
+++ Epam_Task3/Person.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam_Task3
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+        private IEnumerable<string> phoneNumbers;
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+        public IEnumerable<string> PhoneNumbers { get => phoneNumbers; set => phoneNumbers = value; }
+
+
+        public Person()
+        {
+            name = "";
+            age = 0;
+        }
+        public Person(string _name, int _age , IEnumerable<string> _phoneNumbers)
+        {
+            name = _name;
+            age  = _age;
+            phoneNumbers = _phoneNumbers;
+        }
+        public void PrintAll()
+        {
+            Console.Write("\n Name " + Name + " Age " + Age + " PhoneNumbers : ");
+            foreach (var item in phoneNumbers)
+            {
+                Console.Write(item + " ,"); ;
+            }
+           
+        }
+        public void PrintNumbers()
+        {
+            Console.Write("\n"+ Name+ " - ");
+            foreach (var item in phoneNumbers)
+            {
+                Console.Write(item + " ,"); ;
+            }
+
+        }
+
+
+
+
+
+    }
+
+}
---
+++ Epam_Task3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Epam_Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person("person1", 23, new List<string>() { "123456", "123567", "1111111" });
+            Person person2 = new Person("person2", 25, new List<string>() { "123456", "123567" });
+            Person person3 = new Person("person3", 27, new List<string>() { "123456", "123567" });
+            Person person4 = new Person("person4", 22, new List<string>() { "123456", "123567", "44444444" });
+            Person person5 = new Person("person5", 30, new List<string>() { "123456", "123567" });
+            Person person6 = new Person("person6", 19, new List<string>() { "123456", "123567" });
+           
+
+       
+            List<Person> people = new List<Person>();
+            people.Add(person1);
+            people.Add(person2);
+            people.Add(person3);
+            people.Add(person4);
+            people.Add(person5);
+            people.Add(person6);
+
+
+            foreach (var item in people)
+            {
+                item.PrintAll();
+            }
+
+            Person person7 = new Person("person7", 30, new List<string>() { "123456", "123567" });
+            Person person8 = new Person("person8", 19, new List<string>() { "123456", "123567" ,"3456789" });
+            List<Person> newPeople = new List<Person>();
+            newPeople.Add(person7);
+            newPeople.Add(person8);
+
+            people.AddRange(newPeople);
+
+            foreach (var item in people)
+            {
+                item.PrintNumbers();
+            }
+
+        } }
+
+}
